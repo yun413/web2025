3bd0b17a4fbab37c17785d0b9e6107db4dea1b4b
yun413 <wangyunting1024@gmail.com>
2025-11-05 16:10:56 +0800

    251105

--- bs5vue.js
+++ bs5vue.js
@@ -50,3 +50,11 @@ createApp({
         }
     }
 }).mount("#portfolio");
+
+//不是這個文件的內容
+//server.get("/services", (req,res)=>{     //db      
+// var Services= [                 
+// {icon:'fa-shopping-cart', title:'E-Commerce', text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.'},                 
+// {icon:'fa-laptop', title:'Responsive Design', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},                 
+// {icon:'fa-lock', title:'Web Security', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'}             
+// ]     res.send(Services); })
