f487ef4eaaf73ef03e6e33702b8afe14770cb56d
yun413 <wangyunting1024@gmail.com>
2025-11-05 15:58:25 +0800

    251105

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var server = express();
+var bodyParser = require("body-parser");
+
+server.use(express.static(__dirname+"/Public")); 
+server.use(bodyParser.urlencoded());
 
 server.get("/", (req,res)=>{
-    res.setDefaultEncoding("Hello World!");
+    res.send("Hello World!");
+})
+
+server.get("/about", (req,res)=>{
+    res.send("NodeJS");
 })
 
 server.listen(8080)
