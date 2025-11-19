d4543e6763714abfb10cb954fa9caa073fd1cb75
yun413 <wangyunting1024@gmail.com>
2025-11-19 15:59:39 +0800

    251119

--- server.js
+++ server.js
@@ -5,12 +5,36 @@ var bodyParser = require("body-parser");
 server.use(express.static(__dirname+"/Public")); 
 server.use(bodyParser.urlencoded());
 
+var DB = require("nedb-promises");
+var ServiceDB = DB.create(__dirname+"/Service.db");
+
 server.get("/", (req,res)=>{
     res.send("Hello World!");
 })
 
-server.get("/about", (req,res)=>{
-    res.send("NodeJS");
+server.get("/services", (req,res)=>{
+
+    ServiceDB.find({},{_id:0}).then(results=>{
+        res.send(results);
+    }).catch(error=>{
+
+    })
+})
+
+server.get("/portfolio", (req, res) => {
+    var Portfolio = [
+        { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+        { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+        { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+        { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+        { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+        { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+    ]
+    res.send(Portfolio);
+})
+
+server.get("/about", (req, res) => {
+    res.send("Welcome " + req.query.user + " to My first NodeJS server!");
 })
 
 server.listen(8080)
